037a25d6605f6ca144bbdbb682758557d0ec4491
tchuanromanee <tchuanromanee@yahoo.com>
2017-09-11 17:31:13 -0400

    Quantify training dataset

--- lstm_train.py
+++ lstm_train.py
@@ -5,12 +5,31 @@ from keras.models import Sequential
 from keras.layers import Dense, Embedding
 from keras.layers import LSTM
 from keras.datasets import imdb
+import csv
+import random
 
 max_features = 20000
 maxlen = 80 # cut text after this number of words
 batch_size = 32
 
 print('loading data...')
+
+# Get the data
+with open('inputs.csv', 'r') as csvFile:
+	reader = csv.reader(csvFile)
+	trainingSet = list(reader)
+random.shuffle(trainingSet)
+train_data = trainingSet[:250]
+test_data = trainingSet[250:]
+
+
+training_feats = [featSet[:len(featSet)-1] for featSet in train_data]
+training_labels = [labelSet[len(labelSet)-1] for labelSet in train_data]
+	
+print(training_feats)
+print('labels')
+print(training_labels)
+#labels = trainingSet.ix[:,0].values.astype('int32')
 
 #(x_train, y_train), (x_test, y_test) = load_data(num_words=max_features)
 #print(len(x_train), 'train sequences')
