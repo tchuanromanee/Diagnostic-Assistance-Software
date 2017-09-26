2b5cecf277ebeba6333a3939ef3f29e1f9b5ad22
tchuanromanee <tchuanromanee@yahoo.com>
2017-09-26 17:05:29 -0400

    Add models for symptoms, differential diagnoses, and symptoms-diagnostics

--- Web/diagassist/models.py
+++ Web/diagassist/models.py
@@ -13,8 +13,33 @@ class Diagnoses(models.Model):
     def __str__(self):
         return self.name
     # def was_published_recently(self):
-    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1c
 
+	
+class  Diagsymp(models.Model): # Relates which diagnoses has which symptoms
+	linkID = models.AutoField(primary_key=True)
+	diagID = models.ForeignKey(Diagnoses, on_delete=models.CASCADE)
+	symp1 = models.IntegerField()
+	prevalent1 = models.BooleanField()
+	symp2 = models.IntegerField()
+	prevalent2 = models.BooleanField()
+	symp3 = models.IntegerField()
+	prevalent3 = models.BooleanField()
+	symp4 = models.IntegerField()
+	prevalent4 = models.BooleanField()
+	symp5 = models.IntegerField()
+	prevalent5 = models.BooleanField()
+	symp6 = models.IntegerField()
+	prevalent6 = models.BooleanField()
+
+class DiffDiag(models.Model): # Relates each diagnosis to a differential diagnostic
+	diffID = models.AutoField(primary_key=True)
+	diagnosis = models.ForeignKey(Diagnoses, on_delete=models.CASCADE, related_name='diagnosis')
+	differentialDiagnosis = models.ForeignKey(Diagnoses, on_delete=models.CASCADE, related_name='differentialDiagnosis')
+
+class Symptoms(models.Model):
+	sympID = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=500)
 # class Choice(models.Model):
 #     diagnosis = models.ForeignKey(Diagnoses, on_delete=models.CASCADE)
 #     choice_text = models.CharField(max_length=200)
---
+++ Web/diagassist/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+app_name = 'diagassist'
+#urlpatterns = [
+    # ex: /polls/
+#    url(r'^$', views.index, name='index'),
+#]
