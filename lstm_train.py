e30ac32c8f1db31682837e1da4fb7af96805c47b
tchuanromanee <tchuanromanee@yahoo.com>
2017-09-27 14:40:35 -0400

    Reconfigure to use assigned disorder IDs instead of ICD9 codes

--- lstm_train.py
+++ lstm_train.py
@@ -4,11 +4,12 @@ from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Embedding, Flatten, Dropout
 from keras.layers import LSTM
+from keras import layers
 from keras.datasets import imdb
 import csv
 import random
 from keras import metrics
-from numpy import asarray
+from numpy import asarray, newaxis
 
 max_features = 20000
 #maxlen = 80 # cut text after this number of words
@@ -17,7 +18,7 @@ batch_size = 250
 print('loading data...')
 
 # Get the data
-with open('inputs.csv', 'r') as csvFile:
+with open('inputs_catformat.csv', 'r') as csvFile:
 	reader = csv.reader(csvFile)
 	trainingSet = list(reader)
 random.shuffle(trainingSet)
@@ -30,26 +31,28 @@ training_feats = [featSet[:len(featSet)-1] for featSet in train_data]
 # Extract training labels
 training_labels = [labelSet[len(labelSet)-1] for labelSet in train_data]
 # Convert labels to int for classification
-training_ints = [int(float(i) * 100) for i in training_labels]
+#training_ints = [int(float(i) * 100) for i in training_labels]
 # Preprocess categical labels
-training_labels = keras.utils.to_categorical(training_ints, num_classes=None)	
+training_labels = keras.utils.to_categorical(training_labels, num_classes=None)	
 
 testing_feats = [featSet[:len(featSet)-1] for featSet in test_data]
 # Extract testing labels
 testing_labels = [labelSet[len(labelSet)-1] for labelSet in test_data]
 # Convert labels (ICD9 codes) to int for clasification
-testing_ints = [int(float(i) * 100) for i in testing_labels]
-testing_labels = keras.utils.to_categorical(testing_ints, num_classes=None)
+#testing_ints = [int(float(i) * 100) for i in testing_labels]
+testing_labels = keras.utils.to_categorical(testing_labels, num_classes=None)
 
 # Convert to numpy arrays
 training_feats = asarray(training_feats)
+#training_feats = training_feats.reshape(training_feats.shape[0],15)
+#training_feats = training_feats[:,:,newaxis]i
+#training_feats = training_feats[0]
 training_labels = asarray(training_labels)
 testing_feats = asarray(testing_feats)
 testing_labels = asarray(testing_labels)
 
 
 num_features = training_feats.shape[1]
-
 
 # Train dataset
 
@@ -61,23 +64,29 @@ model = Sequential()
 #model.add(Dense(128, input_dim = num_features))
 
 ## Curent testing
-#model.add(LSTM(75, input_shape = (len(training_feats), num_features), dropout=0.2, return_sequences=True))
 #model.add(Embedding(128,15))
-#model.add(Dropout(0.5))
-#model.add(LSTM(78840, dropout=0.2, recurrent_dropout=0.2))
+#
+#model.add(Dense(100, batch_input_shape=(batch_size,15)))
 
-model.add(Dense(78840, input_shape=(15,)))
+model.add(Dense(50, batch_size=batch_size,activation='relu', input_dim=15))
+model.add(Dense(19, activation='softmax'))
+
+#model.add(LSTM(100, batch_input_shape=(batch_size,15,1), return_sequences=True))
+#model.add(layers.Activation('softmax'))
+
+#model.add(Dense(78840, input_shape=(15,)))
 #model.add(Dropout(0.5))
 #model.add(LSTM(50, return_sequences=True))
 #model.add(Flatten())
 #model.add(Dense(78840))
 
+model.summary()
 
 # try using different optimizers and different optimizer configs
 model.compile(loss='categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
 
 print('Train...')
 model.fit(training_feats, training_labels, batch_size=batch_size, epochs=10, validation_data=(testing_feats, testing_labels))
-score, acc = model.evaluate(testing_feats,testing_labels, batch_size=batch_size)
+score, acc = model.evaluate(testing_feats,testing_labels, batch_size=50)
 print('Test score:', score)
 print('Test accuracy:', acc)
