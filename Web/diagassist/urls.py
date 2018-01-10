da23387e341eca696413725c60f019ac6d672398
tchuanromanee <tchuanromanee@yahoo.com>
2018-01-10 12:25:15 -0500

    Add view for index

--- Web/diagassist/urls.py
+++ Web/diagassist/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # ex: /polls/
     #url(r'^$', views.index, name='index'),
     # ex: /polls/
+	url(r'^$', views.indexss, name='main-view')
     #url(r'^$', views.IndexView.as_view(), name='index'),
         # ex: /polls/5/
     #url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
--- Web/diagassist/views.py
+++ Web/diagassist/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+# Create your views here.
+from .models import Therapist, Symptom, Diagnostic
+
+
+class IndexView(generic.ListView):
+	template_name = 'index.html'
+
+def indexss(request):
+    return render(request, 'index.html')
 
 # Create your views here.
 def update_profile(request, user_id):
