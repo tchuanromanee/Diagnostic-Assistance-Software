efff434658490ec23aaa97a3c4419913d0e69990
tchuanromanee <tchuanromanee@yahoo.com>
2018-01-29 14:16:34 -0500

    Resove unknown identifier for Diagnosis Form error

--- Web/diagassist/views.py
+++ Web/diagassist/views.py
@@ -27,6 +27,7 @@ def diagnoseView(request):
 			form.save()
 			#username = form.cleaned_data.get('username')
 			#raw_password = form.cleaned_data.get('password1')
+			# Call function to diagnose
 			return redirect('/diagassist/')
 	else:
 		form = DiagnoseForm()
