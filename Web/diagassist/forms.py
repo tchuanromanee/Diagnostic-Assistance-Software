676b86345dad42872a108d2c194fe90756eaec68
tchuanromanee <tchuanromanee@yahoo.com>
2018-01-31 12:48:53 -0500

    Add selection for preexisting conditions

--- Web/diagassist/forms.py
+++ Web/diagassist/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Diagnostic
 
 
 class SignUpForm(UserCreationForm):
@@ -21,9 +22,11 @@ class DiagnoseForm(forms.Form):
 	# Gender
 	gender = forms.CharField(label='Patient gender', widget=forms.Select(choices=genderChoices))
 	# Age
+	age = forms.IntegerField()
 	# Pre-existing Conditions
+	preexisting = forms.ModelMultipleChoiceField(queryset=Diagnostic.objects.all())
 	# Symptom 1
 	# Persistent 1
-	first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-	last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-	email = forms.EmailField(max_length=254, help_text='Required.')
+	#first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	#last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	#email = forms.EmailField(max_length=254, help_text='Required.')
