9cffe464baccc95f7bcddd53544e19c7f7a3cb6d
tchuanromanee <tchuanromanee@yahoo.com>
2018-01-31 12:44:15 -0500

    Add dropdown menu for gender

--- Web/diagassist/forms.py
+++ Web/diagassist/forms.py
@@ -12,12 +12,18 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
 
+genderChoices = [
+	('m', 'Male'),
+	('f', 'Female'),
+	('x', 'Other'),
+	]
 class DiagnoseForm(forms.Form):
 	# Gender
+	gender = forms.CharField(label='Patient gender', widget=forms.Select(choices=genderChoices))
 	# Age
 	# Pre-existing Conditions
 	# Symptom 1
 	# Persistent 1
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required.')
+	first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+	email = forms.EmailField(max_length=254, help_text='Required.')
