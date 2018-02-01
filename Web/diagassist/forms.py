1da62568bcfbab0fb43ffbae267072d1d7021203
tchuanromanee <tchuanromanee@yahoo.com>
2018-02-01 11:00:33 -0500

    Add 2nd-6th symptoms/persistence

--- Web/diagassist/forms.py
+++ Web/diagassist/forms.py
@@ -32,15 +32,33 @@ class DiagnoseForm(forms.Form):
 	#email = forms.EmailField(max_length=254, help_text='Required.')
 	class Meta:
 		model = Diagnostic
-		fields = ('__all__')
-		widgets = {
-			'name': autocomplete.ModelSelect2(url='diagnose')
-		}
+		fields = ('diagID', 'name')
+
 class DiagnoseSympForm(forms.Form):
 	# Symptom 1
 	symp1 = forms.ModelChoiceField(queryset=Symptom.objects.all(), label='Symptom 1')
 	# Persistent 1
 	persistent1 = forms.BooleanField(label='Symptom 1 Persistent?')
+	# Symptom 2
+	symp2 = forms.ModelChoiceField(queryset=Symptom.objects.all(), label='Symptom 2')
+	# Persistent 2
+	persistent2 = forms.BooleanField(label='Symptom 2 Persistent?')
+	# Symptom 3
+	symp3 = forms.ModelChoiceField(queryset=Symptom.objects.all(), label='Symptom 3')
+	# Persistent 3
+	persistent3 = forms.BooleanField(label='Symptom 3 Persistent?')
+	# Symptom 4
+	symp4 = forms.ModelChoiceField(queryset=Symptom.objects.all(), label='Symptom 4')
+	# Persistent 4
+	persistent4 = forms.BooleanField(label='Symptom 4 Persistent?')
+	# Symptom 5
+	symp5 = forms.ModelChoiceField(queryset=Symptom.objects.all(), label='Symptom 5')
+	# Persistent 5
+	persistent5 = forms.BooleanField(label='Symptom 5 Persistent?')
+	# Symptom 6
+	symp6 = forms.ModelChoiceField(queryset=Symptom.objects.all(), label='Symptom 6')
+	# Persistent 6
+	persistent6 = forms.BooleanField(label='Symptom 6 Persistent?')
 	#first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
 	#last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
 	#email = forms.EmailField(max_length=254, help_text='Required.')
